62b2e2807ff2426f10c42cad2b40f533bc0990bf
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-07-15 13:16:29 -0400

    initial User interface set up, includes three cards and a spin button

--- slotMatchingGame/slotMatchingGame/ContentView.swift
+++ slotMatchingGame/slotMatchingGame/ContentView.swift
@@ -17,12 +17,75 @@ struct ContentView: View {
             Rectangle()
                 .foregroundColor(Color(red: 200/255, green: 143/255, blue: 32/255))
                 .edgesIgnoringSafeArea(.all)
+            
+            Rectangle()
+                .foregroundColor(Color(red: 228/255, green: 195/255, blue: 76/255))
+                .rotationEffect(Angle(degrees: 75))
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                
+                //title
+                HStack{
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.green)
+                    
+                    Text("SwiftUI slots")
+                        .bold()
+                        .foregroundColor(.white)
+                    
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.green)
+                }.scaleEffect(2)
+                
+                //credites counter
+                Text("Credits: 1000")
+                    .foregroundColor(.black)
+                    .background(Color.white.opacity(0.5))
+                    .padding(.all,10)
+                    .background(Color.white.opacity(0.5))
+                    .cornerRadius(20)
+                
+                //Cards
+                HStack {
+                    Spacer()
+                    
+                    Image("apple").resizable()
+                        .aspectRatio(1, contentMode:.fit)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                    
+                    Image("apple").resizable()
+                        .aspectRatio(1, contentMode:.fit)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+
+                    Image("apple").resizable()
+                        .aspectRatio(1, contentMode:.fit)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                    Spacer()
+
+                }
+                //Button
+                Button(action: {
+                    //Todo
+                }) {
+                    Text("Spin")
+                    .bold()
+                        .foregroundColor(.white)
+                        .padding(.all, 10)
+                        .background(Color.pink)
+                        
+                }
+            }
+            
         }
         
-        }
     }
+}
 
-  
+
 
 
 
